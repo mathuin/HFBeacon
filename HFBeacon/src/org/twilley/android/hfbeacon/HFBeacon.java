18837f0023c20c9d67f2cb609066d2fd517e20f7
Jack Twilley <mathuin@gmail.com>
2010-12-12 01:18:16 +0000

    Initial import, take two.
    git-svn-id: https://ariel.home/svn/hfbeacon2/trunk@19 6f41a4af-1f25-4f04-ae1f-536bf232968e

---
+++ HFBeacon/src/org/twilley/android/hfbeacon/HFBeacon.java
@@ -0,0 +1,14 @@
+package org.twilley.android.hfbeacon;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HFBeacon extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
