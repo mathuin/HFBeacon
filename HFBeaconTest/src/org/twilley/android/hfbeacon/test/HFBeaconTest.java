cfa81918451df656dbf23c8c4adb1abe43a43887
Jack Twilley <mathuin@gmail.com>
2010-12-12 01:21:27 +0000

    Initial import, plus svn:ignore of bin and gen.
    git-svn-id: https://ariel.home/svn/hfbeacon2/trunk@21 6f41a4af-1f25-4f04-ae1f-536bf232968e

---
+++ HFBeaconTest/src/org/twilley/android/hfbeacon/test/HFBeaconTest.java
@@ -0,0 +1,32 @@
+package org.twilley.android.hfbeacon.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import org.twilley.android.hfbeacon.HFBeacon;
+
+public class HFBeaconTest extends ActivityInstrumentationTestCase2<HFBeacon> {
+    private HFBeacon mActivity;
+    private TextView mView;
+    private String resourceString;
+    
+    public HFBeaconTest() {
+	      super("org.twilley.android.hfbeacon", HFBeacon.class);
+	}
+	
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+        mView = (TextView) mActivity.findViewById(org.twilley.android.hfbeacon.R.id.textview);
+        resourceString = mActivity.getString(org.twilley.android.hfbeacon.R.string.hello);
+    }
+    
+    public void testPreconditions() {
+        assertNotNull(mView);
+    }
+    
+    public void testText() {
+        assertEquals(resourceString,(String)mView.getText());
+    }
+}
